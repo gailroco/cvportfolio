9ccc8a6e7a38b8e29475ed6b2656ff1b90203a0d
Frida Rojas <gailroco@outlook.com>
2021-01-18 09:29:41 -0600

    Add images to src images folder

--- src/mock/data.js
+++ src/mock/data.js
@@ -29,31 +29,58 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img1: 'bsc0_1.jpg',
+    img2: 'bsc0_2.jpg',
     title: 'Output Feedback Linear Control for an Air-based Levitator',
     info: "During my bachelor's degree",
-    info2: ' ',
-    url: 'https://www.youtube.com/watch?v=dgmX2sGMyIk',
+    info2: '',
+    link1: 'https://www.youtube.com/watch?v=dgmX2sGMyIk',
+    link2: '',
+    url:
+      'https://www.researchgate.net/publication/298341144_Control_por_Retroalimentacion_de_Salida_de_un_Sistema_de_Levitacion_de_Aire',
+    urltxt: 'Read more',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'bachelor_1.png',
+    img1: 'bsc1.jpg',
+    img2: 'bsc2.jpg',
     title: 'Instrumentation and control of a wheeled mobile robot type (2,0)',
     info:
       "For my Bachelor's thesis, I designed and programmed in C a low level PID speed control for the wheels velocities using only encoder readings to generate open loop trajectories on a skid-steered vehicle.",
-    info2: ' ',
-    url: 'https://www.youtube.com/watch?v=JtLYsUKMTZ8',
+    info2: '',
+    link1: 'https://www.youtube.com/watch?v=i-g88BYBD6c',
+    link2: 'https://www.youtube.com/watch?v=JtLYsUKMTZ8',
+    url: 'https://eventos.itam.mx/es/node/11558',
+    urltxt: 'Read more',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'msc1.jpg',
+    img1: 'msc1.jpg',
+    img2: 'msc2.jpg',
     title: 'Continuous sliding mode control of differential drive robot',
     info:
       "For my Master's thesis, I designed and programmed a trajectory tracking algorithm on a differential drive robot using a closed loop scheme based on sliding modes control technique using Super-Twisting algorithm.",
     info2: ' ',
-    url: 'https://www.youtube.com/watch?v=bPUWZJW4Bnk',
+    link1: 'https://www.youtube.com/watch?v=bPUWZJW4Bnk',
+    link2: 'https://www.youtube.com/watch?v=lp1aQLZVq9M',
+    url: 'https://ieeexplore.ieee.org/document/8264571',
+    urltxt: 'Read more',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img1: 'personal1.jpg',
+    img2: 'personal2.jpg',
+    title: 'My own R2-D2',
+    info:
+      "For my own fun, I am building and programming what is going to be a R2-D2 cousin; it's a work in progress. It's a hot mess for now.",
+    info2: ' ',
+    link1: 'https://www.youtube.com/watch?v=bPUWZJW4Bnk',
+    link2: 'https://www.youtube.com/watch?v=lp1aQLZVq9M',
+    url: 'https://ieeexplore.ieee.org/document/8264571',
+    urltxt: 'Read more',
     repo: '', // if no repo, the button will not show up
   },
 ];
