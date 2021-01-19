1fc30c73bfded2602a9a591d21697485461e7aa5
Frida Rojas <gailroco@outlook.com>
2021-01-18 20:21:21 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a2057992b36341478424.js"
+    "url": "webpack-runtime-8ed90a9b7950d5d790b0.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6bd741c5c5d27b8c9e74a8ba956e8ca8"
+    "revision": "549040c6e116c0f0c16da72b312a0796"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "04cabc8e048432f127d3f6bd288bad39"
+    "revision": "671821395d5ebd863819fc753c9065e1"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
